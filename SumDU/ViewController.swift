c0ffe1301cdc69e728cbf1076e43e1befb97fdb1
Maksym Skliarov <maksym.skliarov@gmail.com>
2015-11-21 12:06:58 +0200

    Initial commit

---
+++ SumDU/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SumDU
+//
+//  Created by Maksym Skliarov on 11/21/15.
+//  Copyright © 2015 iOSonRails. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
