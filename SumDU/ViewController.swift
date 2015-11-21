da3f724137d8a75a0ae6565f447dede570769815
Maksym Skliarov <maksym.skliarov@gmail.com>
2015-11-21 12:34:26 +0200

    Added Cocoapods to project

--- SumDU/ViewController.swift
+++ SumDU/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
