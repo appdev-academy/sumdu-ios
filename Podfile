c04b6b2718a8fc96ca9583bfa9e5303b909916e8
Oleksandr Kysil <gomer0513@gmail.com>
2019-10-04 13:00:13 +0300

    Integrated Firebase and Crashlytics.

--- Podfile
+++ Podfile
@@ -5,6 +5,11 @@ abstract_target 'SumDU App' do
   pod 'Alamofire'
   pod 'Cartography'
   pod 'SwiftyJSON'
+  
+  pod 'Fabric', '~> 1.10.2'
+  pod 'Crashlytics', '~> 3.14.0'
+  pod 'Firebase/Analytics'
+  
   pod 'Fuzi'
   target 'SumDU'
 end
--- SumDU/App Delegate/AppDelegate.swift
+++ SumDU/App Delegate/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2015 AppDevAcademy. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 @UIApplicationMain
@@ -14,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    // Configure Firebase
+    FirebaseApp.configure()
     
     self.window = UIWindow(frame: UIScreen.main.bounds)
     
